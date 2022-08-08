1f8fe995c3eabe1027ad978342c39ac4d65b70a2
张杰 <googlegis@hotmail.com>
2022-08-08 17:25:24 +0800

    initial

---
+++ src/chapter-01/js/01-03.js
@@ -0,0 +1,153 @@
+function init() {
+    console.log("Using Three js version:" + THREE.REVISION)
+
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setClearColor(new THREE.Color(0x000000));
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.shadowMap.enabled = true;
+
+    createTree(scene);
+    createHouse(scene);
+    createGroundPlane(scene);
+    createBoudingWall(scene);
+
+    var axes = new THREE.AxesHelper(20);
+    scene.add(axes);
+
+    var planeGeometry = new THREE.PlaneGeometry(60,20);
+    var planeMaterial = new THREE.MeshBasicMaterial({
+        color: 0xAAAAAA
+    });
+    var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+    plane.rotation.x = -0.5 * Math.PI;
+    plane.position.set(15,0,0);
+    plane.receiveShadow = true;
+    scene.add(plane)
+
+    var cubeGeometry = new THREE.BoxGeometry(4,4,4);
+    var cubeMaterial = new THREE.MeshBasicMaterial({
+        color: 0xFF0000,
+        wireframe: true
+    });
+    var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+    cube.castShadow = true;
+    cube.position.set(-4,2,0);
+    scene.add(cube);
+
+    var sphereGeometry = new THREE.SphereGeometry(4,20,20);
+    var sphereMaterial = new THREE.MeshLambertMaterial({
+        color: 0x7777FF
+    })
+    var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+    sphere.position.set(20,4,2);
+    sphere.castShadow = true;
+    scene.add(sphere);
+
+    camera.position.set(-30, 40, 30);
+    camera.lookAt(scene.position);
+
+    var spotLight = new THREE.SpotLight(0xFFFFFF);
+    spotLight.position.set(-40,40,-15);
+    spotLight.castShadow = true;
+    spotLight.shadow.mapSize = new THREE.Vector2(1024, 1024);
+    spotLight.shadow.camera.far = 130;
+    spotLight.shadow.camera.near = 40;
+    scene.add(spotLight);
+
+    var ambienLight = new THREE.AmbientLight(0x353535);
+    scene.add(ambienLight);
+
+    document.getElementById("webgl-output").appendChild(renderer.domElement);
+    renderer.render(scene, camera);
+}
+
+function createBoudingWall(scene) {
+    var wallLeft = new THREE.BoxGeometry(70,2,2);
+    var wallRight = new THREE.BoxGeometry(70, 2, 2);
+    var wallTop = new THREE.BoxGeometry(2,2,50);
+    var wallBottom = new THREE.BoxGeometry(2,2,50);
+
+    var wallMaterial = new THREE.MeshLambertMaterial({
+        color: 0xa0522d
+    });
+
+    var wallLeftMesh = new THREE.Mesh(wallLeft, wallMaterial);
+    var wallRightMesh = new THREE.Mesh(wallRight, wallMaterial);
+    var wallTopMesh = new THREE.Mesh(wallTop, wallMaterial);
+    var wallBottomMesh = new THREE.Mesh(wallBottom, wallMaterial);
+
+    wallLeftMesh.position.set(15,1,-25);
+    wallRightMesh.position.set(15,1,25);
+    wallTopMesh.position.set(-19,1,0);
+    wallBottomMesh.position.set(49,1,0);
+
+    scene.add(wallLeftMesh);
+    scene.add(wallRightMesh);
+    scene.add(wallBottomMesh);
+    scene.add(wallTopMesh);
+}
+
+function createGroundPlane(scene) {
+    var planeGeometry = new THREE.PlaneGeometry(70,50);
+    var planeMaterial = new THREE.MeshLambertMaterial({
+        color: 0x9acd32
+    });
+    var plane = new THREE.Mesh(planeGeometry,planeMaterial);
+    plane.receiveShadow = true;
+
+    plane.rotation.x = -0.5 * Math.PI;
+    plane.position.x = 15;
+    plane.position.y = 0;
+    plane.position.z = 0;
+    scene.add(plane)
+}
+
+function createHouse(scene) {
+    var roof = new THREE.ConeGeometry(5,4);
+    var base = new THREE.CylinderGeometry(5,5,6);
+
+    var roofMesh = new THREE.Mesh(roof, new THREE.MeshLambertMaterial({
+        color: 0x8b7213
+    }));
+
+    var baseMesh = new THREE.Mesh(base, new THREE.MeshLambertMaterial({
+        color: 0xffe4c4
+    }))
+
+    roofMesh.position.set(25, 8, 0);
+    baseMesh.position.set(25, 3, 0);
+
+    roofMesh.receiveShadow = true;
+    baseMesh.receiveShadow = true;
+    roofMesh.castShadow = true;
+    baseMesh.castShadow = true;
+
+    scene.add(roofMesh);
+    scene.add(baseMesh);
+}
+
+function createTree(scene) {
+    var trunk = new THREE.BoxGeometry(1,8,1);
+    var leaves = new THREE.SphereGeometry(4);
+
+    var trunkMesh = new THREE.Mesh(trunk, new THREE.MeshLambertMaterial({
+        color: 0x8b4513
+    }));
+    var leavesMesh = new THREE.Mesh(leaves, new THREE.MeshLambertMaterial({
+        color: 0x00ff00
+    }));
+
+    trunkMesh.position.set(-10,4,0);
+    trunkMesh.castShadow = true;
+    trunkMesh.receiveShadow = true;
+
+    leavesMesh.position.set(-10,4,0);
+    leavesMesh.castShadow = true;
+    leavesMesh.receiveShadow = true;
+
+    scene.add(trunkMesh);
+    scene.add(leavesMesh);
+}
---
+++ src/chapter-01/js/01-04.js
@@ -0,0 +1,84 @@
+function init() {
+
+    console.log("Using Three js version:" + THREE.REVISION)
+
+    var stats = initStats();
+
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+    var renderer = new THREE.WebGLRenderer();
+
+    renderer.setClearColor(new THREE.Color(0x000000));
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.shadowMap.Enabled = true;
+
+    var planeGeometry = new THREE.PlaneGeometry(60,20, 1, 1);
+    var planeMaterial = new THREE.MeshLambertMaterial({
+        color: 0xffffff
+    });
+    var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+    plane.receiveShadow = true;
+
+    plane.rotation.x = -0.5 * Math.PI;
+    plane.position.x = 15;
+    plane.position.y = 0;
+    plane.position.z = 0;
+    scene.add(plane)
+
+    var cubeGeometry = new THREE.BoxGeometry(4,4,4);
+    var cubeMaterial = new THREE.MeshLambertMaterial({
+        color: 0xFF0000
+    });
+    var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+    cube.castShadow = true;
+
+    cube.position.x = -4;
+    cube.position.y = 4;
+    cube.position.z = 0;
+    scene.add(cube);
+
+    var sphereGeometry = new THREE.SphereGeometry(4,20,20);
+    var sphereMaterial = new THREE.MeshLambertMaterial({
+        color: 0x7777FF
+    })
+    var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+    sphere.position.set(20,0,2);
+    sphere.castShadow = true;
+    scene.add(sphere);
+
+
+    camera.position.set(-30, 40, 30);
+    camera.lookAt(scene.position);
+
+    var ambienLight = new THREE.AmbientLight(0x353535);
+    scene.add(ambienLight);
+
+    var spotLight = new THREE.SpotLight(0xFFFFFF);
+    spotLight.position.set(-10,20,-5);
+    spotLight.castShadow = true;
+    // spotLight.shadow.mapSize = new THREE.Vector2(1024, 1024);
+    // spotLight.shadow.camera.far = 130;
+    // spotLight.shadow.camera.near = 40;
+    scene.add(spotLight);
+
+    document.getElementById("webgl-output").appendChild(renderer.domElement);
+
+    var step = 0;
+    renderScene();
+
+    function renderScene() {
+
+        stats.update();
+
+        cube.rotation.x += 0.02;
+        cube.rotation.y += 0.02;
+        cube.rotation.z += 0.02;
+
+        step += 0.04;
+        sphere.position.x = 20 + (10 * (Math.cos(step)));
+        sphere.position.y = 2 + (10 * Math.abs(Math.sin(step)));
+
+        requestAnimationFrame(renderScene)
+        renderer.render(scene, camera);
+    }
+}
