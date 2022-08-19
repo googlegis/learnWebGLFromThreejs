e00b0d5c5aa614dc5c0ff2d7f16abe58b11ba6dd
张杰 <googlegis@hotmail.com>
2022-08-19 11:20:58 +0800

    chapter-07

---
+++ src/chapter-07/js/07-01.js
@@ -0,0 +1,45 @@
+function init() {
+
+    // use the defaults
+    let stats = initStats();
+    let renderer = initRenderer()
+    let camera = initCamera();
+    let clock = new THREE.Clock();
+    var trackballControls = initTrackballControls(camera, renderer);
+
+    // create a scene, that will hold all our elements such as objects, cameras and lights.
+    let scene = new THREE.Scene();
+    camera.position.x = 0;
+    camera.position.y = 0;
+    camera.position.z = 150;
+
+    camera.lookAt(new THREE.Vector3(0,0,0));
+
+    createSprites();
+
+    render();
+
+    function createSprites() {
+        for(let x = -15; x < 15; x++) {
+            for( let y = -10; y < 10; y++) {
+                let material = new THREE.SpriteMaterial({
+                    color: Math.random() * 0xffffff
+                });
+
+                let sprite = new THREE.Sprite(material);
+                sprite.position.set(x * 4, y * 4, 0);
+                scene.add(sprite);
+            }
+        }
+    }
+
+    function render() {
+        stats.update();
+
+        trackballControls.update(clock.getDelta());
+
+        // render using requestAnimationFrame
+        requestAnimationFrame(render);
+        renderer.render(scene, camera);
+    }
+}
